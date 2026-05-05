7bca0fa8b894c3fe1003a7ba0f67137bb5482563
Urs Lang <urs.lang@luzern.com>
2026-05-05 21:29:02 +0200

    .

--- src/Json/Job/JobJsonReader.php
+++ src/Json/Job/JobJsonReader.php
@@ -2,7 +2,6 @@
 
 namespace LuzernTourismus\Pixxio\Json\Job;
 
-use LuzernTourismus\Pixxio\Json\Base\AbstractJsonPixxioReader;
 use LuzernTourismus\Pixxio\Json\MediaspaceConfigTrait;
 use LuzernTourismus\Pixxio\WebRequest\PixxioWebRequest;
 use Nemundo\Core\Base\AbstractBase;
@@ -11,27 +10,20 @@ use Nemundo\Core\Debug\Debug;
 class JobJsonReader extends AbstractBase
 {
 
-use MediaspaceConfigTrait;
+    use MediaspaceConfigTrait;
 
     public function getJob($jobId)
     {
-
 
         $request = new PixxioWebRequest();
         $request->subdomain = $this->subdomain;
         $request->apiKey = $this->apiKey;
 
-        $response = $request->getData('jobs/'.$jobId,'');
+        $response = $request->getData('jobs/' . $jobId, '?responseFields=id&responseFields=jobData&responseFields=error&responseFields=jobType');
 
         (new Debug())->write($response);
 
-        //$jobId=22920798;
-
-        //$this->endpoint = 'jobs/'.$jobId;
-
-
     }
-
 
 
     protected function onJson($json)
@@ -40,15 +32,5 @@ use MediaspaceConfigTrait;
         (new Debug())->write($json);
 
     }
-
-
-
-
-    /*
-$request = new PixxioWebRequest();
-$request->fromMediaspaceConfig(new LuzernMediaspaceConfig());
-$data = $request->getData('jobs/'.$jobId,'');
-(new Debug())->write($data);
-    */
 
 }
