65bac9e926dfcf6874f36f84aaafc1c59e72686f
Urs Lang <urs.lang@luzern.com>
2026-06-22 18:38:13 +0200

    .

--- src/Site/WebhookRequestSite.php
+++ src/Site/WebhookRequestSite.php
@@ -36,78 +36,70 @@ class WebhookRequestSite extends AbstractSite
             ob_start();
 
             $json = file_get_contents('php://input');
-            $object = json_decode($json);
+            //$object = json_decode($json);
 
 
-            try {
+            //try {
 
-            $reader = new JsonReader();
-            $reader->fromText($json);
-            $jsonData = $reader->getData();
+                $reader = new JsonReader();
+                $reader->fromText($json);
+                $jsonData = $reader->getData();
 
-            $eventData = $jsonData['events'];   // $ json_decode(json_encode($eventData), true);
+                $eventData = $jsonData['events'];   // $ json_decode(json_encode($eventData), true);
 
-            (new Debug())->write($eventData);
+                //(new Debug())->write($eventData);
 
 
-            /*[events] => Array
-            (
-                [0] => Array
+                /*[events] => Array
                 (
-                    [id] => 2098540229
-                    [eventKey] => commentCreated
-            [action] => created
-            [name] => createComment
-            [createDate] => 2026-06-22 18:30:19
-                    [modifyDate] => 2026-06-22 18:30:19
-                    [applicationKe*/
+                    [0] => Array
+                    (
+                        [id] => 2098540229
+                        [eventKey] => commentCreated
+                [action] => created
+                [name] => createComment
+                [createDate] => 2026-06-22 18:30:19
+                        [modifyDate] => 2026-06-22 18:30:19
+                        [applicationKe*/
 
 
+                $data = new Webhook();
+                $data->id = $eventData['id'];
+                $data->dateTime = new DateTime($eventData['createDate']);
+                $data->actionName = $eventData['name'];
 
-            $data = new Webhook();
-            $data->id = $jsonData['id'];
-            $data->dateTime = new DateTime($jsonData['createDate']);
-            $data->actionName = $jsonData['name'];
+                if (isset($eventData['data']['fileID'])) {
+                    $data->fileId = $eventData['data']['fileID'];
+                }
 
-            if (isset($data['data']['fileID'])) {
-                $data->fileId = $data['data']['fileID'];
+                $data->save();
+
+
+                /*   if (json_last_error() !== JSON_ERROR_NONE) {
+
+                   }
+
+                   //$filename = (new TmpPath())->addPath('webhook2.json')->getFullFilename();
+
+                   //file_put_contents($filename, print_r($object, true));*/
+
+                /*$content = ob_get_contents();
+
+                $file = new TextFileWriter((new TmpPath())->addPath('webhook.txt')->getFullFilename());
+                $file->overwriteExistingFile = true;
+                $file->addLine($json);
+                $file->addLine($content);
+                $file->writeFile();*/
+
+            } else {
+
+                (new Debug())->write('Wrong Request');
+
             }
-
-            $data->save();
-
-
-            } catch (\Exception $ex) {
-                (new Debug())->write($ex->getMessage());
-            }
-
-
-
-
-         /*   if (json_last_error() !== JSON_ERROR_NONE) {
-
-            }
-
-            //$filename = (new TmpPath())->addPath('webhook2.json')->getFullFilename();
-
-            //file_put_contents($filename, print_r($object, true));*/
-
-            $content = ob_get_contents();
-
-            $file = new TextFileWriter((new TmpPath())->addPath('webhook.txt')->getFullFilename());
-            $file->overwriteExistingFile = true;
-            $file->addLine($json);
-            $file->addLine($content);
-            $file->writeFile();
-
-        } else {
-
-            (new Debug())->write('Wrong Request');
-
-        }
 
 
 //$response = new PostRequest()
 
 
     }
-}
+    }
